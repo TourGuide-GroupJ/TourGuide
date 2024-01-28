92b2f4a1b7bfa192e14bba7c075fde4dcab0e867
shanaka <shanakasenanayaka98@gmail.com>
2024-01-28 17:33:27 +0530

    updateRentalItems

--- frontend/src/pages/RentItems.jsx
+++ frontend/src/pages/RentItems.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
+
+// import campingTent from "./assests/campingTent"
+
 
 const RentItems = () => {
   return (
     <div>RentItems</div>
+    
   )
 }
 
